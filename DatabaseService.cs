bf16f977b536bc47c6c5c8ceddc47728645a62bc
Gregory Demirdjian <gregoryd2003@gmail.com>
2025-02-01 13:54:42 -0500

    Initial commit for backend

---
+++ DatabaseService.cs
@@ -0,0 +1,32 @@
+﻿namespace fl_backend
+{
+    using MySql.Data.MySqlClient;
+    using Microsoft.Extensions.Configuration;
+    using System;
+
+    public class DatabaseService
+    {
+        private readonly string _connectionString;
+
+        public DatabaseService(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("MySqlDatabase");
+        }
+
+        public void TestConnection()
+        {
+            try
+            {
+                using (var connection = new MySqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection successful!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
