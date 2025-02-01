7057a8cda7feca7e4689b382bcb11e1f8375566b
Alec Kirakossian <aleckirakossian@yahoo.ca>
2025-02-01 17:33:10 -0500

    Finished Home Page, all other pages created

---
+++ ConUHacks-IX/src/FHSA.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FHSA = () => {
+    return (
+        <div>
+            <h1>FHSA Page</h1>
+            <p>Welcome to the FHSA page. This is a template for your new component.</p>
+        </div>
+    );
+};
+
+export default FHSA;
